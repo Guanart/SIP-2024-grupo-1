4c52e60ca03daadaf573bb534ba6c27a6a76b391
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-03-28 11:47:42 -0300

    configura rutas con react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+	About,
+	Fundraising,
+	Home,
+	Marketplace,
+	Trending,
+	Wallet,
+} from './pages';
+import './index.css';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: '/about',
+		element: <About />,
+	},
+	{
+		path: '/marketplace',
+		element: <Marketplace />,
+	},
+	{
+		path: '/fundraising',
+		element: <Fundraising />,
+	},
+	{
+		path: '/wallet',
+		element: <Wallet />,
+	},
+	{
+		path: '/trending',
+		element: <Trending />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
