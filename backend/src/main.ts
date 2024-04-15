07791acedc30a1d4911e3a8b67dad505b7d80418
Gonzalo Benito <60377535+Guanart@users.noreply.github.com>
2024-04-15 12:22:23 -0300

    Swagger + create account timestamp migration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+  // Swagger
+  // Use DocumentBuilder to create a new Swagger document configuration
+  const config = new DocumentBuilder()
+    .setTitle('League of Token API')
+    .setDescription("This is the League of Token API. It provides service to React frontend app, and uses Auth0 and MercadoPago services")
+    .setVersion('0.1')
+    .build(); // Build the document
+  // Create a Swagger document using the application instance and the document configuration
+  const document = SwaggerModule.createDocument(app, config);
+  // Setup Swagger module (UI) with the application instance and the Swagger document
+  SwaggerModule.setup('api', app, document);
+
+  // Init
   await app.listen(process.env.PORT);
 }
 
