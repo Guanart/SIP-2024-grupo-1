5fd638f112afbdbb7e03ffde7eef59d2687ca87a
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-04-16 17:07:00 -0300

    fix react-router bug

--- frontend/src/components/Protected/Protected.tsx
+++ frontend/src/components/Protected/Protected.tsx
@@ -2,6 +2,6 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Outlet, Navigate } from 'react-router-dom';
 
 export const Protected = () => {
-	const { isAuthenticated } = useAuth0();
-	return isAuthenticated ? <Outlet /> : <Navigate to='/login' />;
+	const { isAuthenticated, isLoading } = useAuth0();
+	return isAuthenticated || isLoading ? <Outlet /> : <Navigate to='/login' />;
 };
