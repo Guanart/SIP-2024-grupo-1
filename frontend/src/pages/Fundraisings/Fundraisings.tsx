4656bba837573e670096f0be637f9cf5bde8dddc
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-05-01 16:14:26 -0300

    agrega filtrado de colectas

--- frontend/src/pages/Fundraisings/Fundraisings.tsx
+++ frontend/src/pages/Fundraisings/Fundraisings.tsx
@@ -1,15 +1,22 @@
 import { PageLayout } from '../../layouts/PageLayout';
 import { FundraisingCard } from '../../components/fundraisings/FundraisingCard';
-import { Container } from '@mui/material';
+import { Container, Stack, TextField, Typography } from '@mui/material';
 import { useAccessToken } from '../../hooks';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useEffect, useState } from 'react';
 import { fetchWithAuth } from '../../utils/fetchWithAuth';
 import { Fundraising } from '../../types';
+import { Loader } from '../../components';
 
 export const Fundraisings = () => {
 	const { accessToken } = useAccessToken();
 	const [fundraisings, setFundraisings] = useState<Fundraising[]>([]);
+	const [currentFundraisings, setCurrentFundraisings] = useState<
+		Fundraising[]
+	>([]);
+	const [filter, setFilter] = useState<string>('');
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
 	const { user, isAuthenticated } = useAuth0();
 
 	useEffect(() => {
@@ -24,6 +31,8 @@ export const Fundraisings = () => {
 				if (response.ok) {
 					const { fundraisings } = await response.json();
 					setFundraisings(fundraisings);
+					setCurrentFundraisings(fundraisings);
+					setIsLoading(false);
 				}
 			} catch (error) {
 				console.log(error);
@@ -36,22 +45,66 @@ export const Fundraisings = () => {
 		getFundraisings();
 	}, [accessToken, isAuthenticated, user]);
 
+	useEffect(() => {
+		if (filter) {
+			const value = filter.trim().toLowerCase();
+			const nextFilteredFundraisings = fundraisings.filter(
+				(fundraising) => {
+					const event = fundraising.event.name.toLowerCase();
+					const username =
+						fundraising.player.user.username.toLowerCase();
+					const game = fundraising.player.game.name.toLowerCase();
+					return (
+						game.includes(value) ||
+						event.includes(value) ||
+						username.includes(value)
+					);
+				}
+			);
+			setCurrentFundraisings(nextFilteredFundraisings);
+		} else {
+			setCurrentFundraisings(fundraisings);
+		}
+	}, [filter, fundraisings]);
+
 	return (
 		<PageLayout title='Fundraisings'>
-			<Container
-				sx={{
-					marginTop: '18px',
-				}}
-			>
-				{fundraisings.map((fundraising) => {
-					return (
-						<FundraisingCard
-							fundraising={fundraising}
-							key={fundraising.id}
-						/>
-					);
-				})}
-			</Container>
+			{!isLoading ? (
+				<Stack spacing={4} mt={2}>
+					<TextField
+						id='filter'
+						label='Type to filter fundraisings...'
+						variant='outlined'
+						value={filter}
+						onChange={(event) => setFilter(event.target.value)}
+						inputProps={{ maxLength: 80 }}
+						sx={{ maxWidth: '600px', width: '95%' }}
+						type='text'
+					/>
+					<Container
+						sx={{
+							marginTop: '18px',
+						}}
+					>
+						{currentFundraisings.length > 0 ? (
+							currentFundraisings.map((fundraising) => {
+								return (
+									<FundraisingCard
+										fundraising={fundraising}
+										key={fundraising.id}
+									/>
+								);
+							})
+						) : (
+							<Typography>
+								No fundraisings match your search.
+							</Typography>
+						)}
+					</Container>
+				</Stack>
+			) : (
+				<Loader />
+			)}
 		</PageLayout>
 	);
 };
