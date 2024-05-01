e2a8eacf118de50b4e37fb5d51ffa762e8ce586e
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-05-01 11:44:55 -0300

    agrega restricciones al dto

--- backend/src/fundraising/dto/create-fundraising.dto.ts
+++ backend/src/fundraising/dto/create-fundraising.dto.ts
@@ -1,15 +1,20 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Max, Min } from 'class-validator';
 
 export class CreateFundraisingDto {
   @IsNotEmpty()
+  @Min(1)
   goal_amount: number;
 
+  @Min(0)
+  @Max(100)
   @IsNotEmpty()
   prize_percentage: number;
 
+  @Min(1)
   @IsNotEmpty()
   player_id: number;
 
+  @Min(1)
   @IsNotEmpty()
   event_id: number;
 }
