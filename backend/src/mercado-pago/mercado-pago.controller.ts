a0afb3764f8d9ad98ee66eb0e284bede81497d36
Gonzalo Benito <60377535+Guanart@users.noreply.github.com>
2024-05-03 15:05:22 -0300

    Rutas MercadopagoController

--- backend/src/mercado-pago/mercado-pago.controller.ts
+++ backend/src/mercado-pago/mercado-pago.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { MercadoPagoService } from './mercado-pago.service';
 import { CreatePreference } from './create-preference.dto';
 
@@ -11,4 +11,10 @@ export class MercadoPagoController {
   async createPreference(@Body() items: CreatePreference) {
     return this.mercadoPagoService.createPreference(items);
   }
+
+  @Post('webhook') async handleWebhook(@Body() notification: any) {
+    console.log(notification);
+  } // Handle the webhook notification logic here
+
+  @Post('oauth') async authorizeSeller() { } // Handle the seller authorization logic here }
 }
