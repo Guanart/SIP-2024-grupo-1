6e792a377d78008c17db85e717a3eaa2b7974587
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-05-15 21:23:19 -0300

    agrega test unitario marketplace-publication.service

--- backend/tests/marketplace/marketplace-publication.service.spec.ts
+++ backend/tests/marketplace/marketplace-publication.service.spec.ts
@@ -1,5 +1,58 @@
-describe('MarketplaceService', () => {
-  it('should pass', () => {
-    expect(1).toBe(1);
+import { PrismaService } from '../../src/database/prisma.service';
+import { MarketplacePublicationService } from '../../src/marketplace/marketplace-publication.service';
+
+jest.mock('../../src/database/prisma.service', () => ({
+  PrismaService: jest.fn().mockImplementation(() => ({
+    marketplace_publication: {
+      create: jest.fn(),
+      delete: jest.fn(),
+    },
+  })),
+}));
+
+describe('MarketplacePublicationService', () => {
+  let marketplacePublicationService: MarketplacePublicationService;
+  let prisma: PrismaService;
+
+  beforeEach(() => {
+    prisma = new PrismaService();
+    marketplacePublicationService = new MarketplacePublicationService(prisma);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('createMarketplacePublication', () => {
+    it('should create a new marketplace publication', async () => {
+      const newPublication = {
+        price: 10,
+        token_id: 1,
+        out_wallet_id: 1,
+      };
+
+      await marketplacePublicationService.createMarketplacePublication(
+        newPublication,
+      );
+
+      expect(prisma.marketplace_publication.create).toHaveBeenCalledWith({
+        data: newPublication,
+      });
+    });
+  });
+
+  describe('deleteMarketplacePublication', () => {
+    it('should delete a marketplace publication', async () => {
+      const publication_id = 1;
+
+      await marketplacePublicationService.deleteMarketplacePublication(
+        publication_id,
+      );
+
+      expect(prisma.marketplace_publication.delete).toHaveBeenCalledWith({
+        where: { publication_id: publication_id, active: true },
+        include: expect.any(Object),
+      });
+    });
   });
 });
