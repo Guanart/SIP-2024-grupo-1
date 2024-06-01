e85ecf314875274dbda934548f5240ef4b905542
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-06-01 18:49:06 -0300

    agrega opción headless a selenium

--- selenium/webdriver.py
+++ selenium/webdriver.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-def get () :
-    driver = webdriver.Chrome()
-    driver.get("http://34.86.184.119:5173/")
+
+def get():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("http://34.139.226.34:5173/")
+    # driver = webdriver.Chrome()
+    # driver.get("http://localhost:5173/")
+
     return driver
