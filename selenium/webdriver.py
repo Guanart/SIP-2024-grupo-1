599364044262c233baa8099f7647e76b463e6646
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-06-02 11:06:04 -0300

    agrega options para correr selenium en pipeline

--- selenium/webdriver.py
+++ selenium/webdriver.py
@@ -5,8 +5,11 @@ from selenium.webdriver.chrome.options import Options
 def get():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument(
+        "--unsafely-treat-insecure-origin-as-secure=http://myurl.com/")
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get("http://34.139.226.34:5173/")
+    driver.get("http://34.23.248.112:5173/")
+
     # driver = webdriver.Chrome()
     # driver.get("http://localhost:5173/")
 
