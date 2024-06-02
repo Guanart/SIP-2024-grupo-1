78cdcf1623e762e2ef9f8d0d49235a49010e8860
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-06-02 15:02:08 -0300

    fix headless en selenium tests

--- selenium/webdriver.py
+++ selenium/webdriver.py
@@ -1,16 +1,18 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
 def get():
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless=new')
     chrome_options.add_argument(
-        "--unsafely-treat-insecure-origin-as-secure=http://myurl.com/")
+        "--unsafely-treat-insecure-origin-as-secure=http://34.23.248.112:5173")
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("http://34.23.248.112:5173/")
 
     # driver = webdriver.Chrome()
     # driver.get("http://localhost:5173/")
+    time.sleep(10)
 
     return driver
