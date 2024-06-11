2e189c00ad5cbe1aa4371b17e1691792b40d3016
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-06-11 15:10:58 -0300

    (up) fix selenium? :(

--- selenium/run-tests.sh
+++ selenium/run-tests.sh
@@ -6,12 +6,11 @@ FRONTEND_IP=$1
 # Establecer la variable de entorno FRONTEND_IP
 export FRONTEND_IP=$FRONTEND_IP
 
-
-python3 login.py \ 
-python3 account-edit.py \ 
-python3 list-marketplace.py  \ 
-python3 delete-marketplace-publication.py  \
-python3 create-marketplace-publication.py  \ 
-python3 list-fundraising.py  \ 
-python3 update-fundraising.py  \ 
+python3 login.py
+python3 account-edit.py
+python3 list-marketplace.py
+python3 delete-marketplace-publication.py
+python3 create-marketplace-publication.py
+python3 list-fundraising.py
+python3 update-fundraising.py
 python3 start-fundraising.py
